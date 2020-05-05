9bc254aafbb73da90df7b8d2b6a1b27c83e3d480
BingBuLiang <2330181012@qq.com>
2020-05-05 12:20:12 +0800

    :pencil: update content

---
+++ update.sh
@@ -0,0 +1,9 @@
+# 如果没有消息后缀，默认提交信息为 `:pencil: update content`
+# 参考https://www.yunyoujun.cn/share/how-to-build-your-site/#%E5%A4%87%E4%BB%BD%E4%B8%8E%E8%87%AA%E5%8A%A8%E9%83%A8%E7%BD%B2
+info=$1
+if ["$info" = ""];
+then info=":pencil: update content"
+fi
+git add -A
+git commit -m "$info"
+git push origin hexo
